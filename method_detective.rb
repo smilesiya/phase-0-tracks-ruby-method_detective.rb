b448c645a86f21f8d75b2ab333527e1c61f464af
smilesiya <smilesiya11@gmail.com>
2017-08-20 23:07:24 -0500

    Add files via upload

---
+++ method_detective.rb
@@ -0,0 +1,30 @@
+# Replace in the "<???>" with the appropriate method (and arguments, if any).
+# Uncomment the calls to catch these methods red-handed.
+
+# When there's more than one suspect who could have
+# committed the crime, add additional calls to prove it.
+
+iNvEsTiGaTiOn".swapcase 
+# => “InVeStIgAtIoN”
+
+"zom".insert(2, "o")
+# => “zoom”
+
+"enhance".center(20) 
+ 
+"Stop! You’re under arrest!".upcase 
+ 
+"the usual".insert(9, " suspect") 
+ 
+"suspects".insert(1, "the usual ") 
+ 
+"The case of the disappearing last letter"[0...39] 
+
+"The mystery of the missing first letter"[1...40] 
+
+"Elementary,    my   dear        Watson!".squeeze 
+"z".ord 
+
+#(What is the significance of the number 122 in relation to the character z?) 
+#Represents the ASCII character number for the letter "Z" 
+"How many times does the letter 'a' appear in this string?".count('a') 
